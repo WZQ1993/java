5d7593cff3a177a8969b8ff3914cbee1d30e46a3
unknown <>
2016-10-25 00:22:14 +0800

    集合操作

---
+++ src/main/java/com/wangziqing/bitMapDemo/BitMapTest.java
@@ -0,0 +1,16 @@
+package com.wangziqing.bitMapDemo;
+
+import java.util.BitSet;
+
+/**
+ * Created by 王梓青 on 2016/10/25.
+ */
+public class BitMapTest {
+    private void test(){
+        //创建一个一千万bit的bitSet，初始化所有位为false
+        BitSet bitSet=new BitSet(10000000);
+        //比哪里大数据集，并置位
+        bitSet.set(999,true);
+
+    }
+}
---
+++ src/main/java/com/wangziqing/collectionDemo/SortDemo.java
@@ -0,0 +1,8 @@
+package com.wangziqing.collectionDemo;
+
+/**
+ * Created by 王梓青 on 2016/10/25.
+ */
+public class SortDemo {
+
+}
