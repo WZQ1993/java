c45a6237d634ba037adb38a2c8d5f0e52ce4a1ef
ziqingwang <wangziqing@kugou.net>
2016-10-26 18:37:10 +0800

    stream、

--- src/main/java/com/wangziqing/java8Demo/streamAPITest/StreamTest.java
+++ src/main/java/com/wangziqing/java8Demo/streamAPITest/StreamTest.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 
 import java.sql.Array;
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -152,8 +153,8 @@ public class StreamTest {
         //收集到集合中
         //传入的单个参数分别是 （集合构造，添加单个元素，添加集合）
         stream.collect(HashSet::new,HashSet::add,HashSet::addAll);
-        stream.collect(Collectors.toList());
-        stream.collect(Collectors.toSet());
+        stream.collect(Collectors.toList());//default==ArrayList
+        stream.collect(Collectors.toSet());//default==HashSet
         //控制得到的集合类型
         stream.collect(Collectors.toCollection(HashSet::new));
         //将流收集并使用分隔符连接起来
@@ -171,6 +172,74 @@ public class StreamTest {
         //区别，按照流的顺序，放弃并行计算
         stream.forEachOrdered(i->i.toString());
         //以上为终止操作，如果要继续使用流，用peek(),该方法会返回原始流，但是每次获取元素会调用传入的函数。见上文
+    }
+
+    /**
+     * 将结果收集到map中
+     * 默认是HashMap
+     */
+    private void streamOperation_6(){
+        Stream<Integer> stream= Stream.of(0,1,2,3,4,5,6,7,8,9,10);
+        //0.用于并发
+        Map<Integer,String> map0=stream.collect(Collectors.toConcurrentMap(
+                i->i,
+                i->i.toString()
+        ));
+        //1.定义两个函数，一个产生KEY，一个产生VALUE
+        Map<Integer,String> map1=stream.collect(Collectors.toMap(
+                i->i,
+                i->i.toString()
+        ));
+        //2.Function.identity()实际的元素
+        Map<String,Integer> map2=stream.collect(Collectors.toMap(
+                i->i.toString(),
+                Function.identity()
+        ));
+        //3.如果存在相同的key，聚合函数接口BinaryOperator   （旧值，新值）——》处理后的值
+        Map<String,Integer> map3=stream.collect(Collectors.toMap(
+                i->i.toString(),
+                Function.identity(),
+                (existValue,newValue)->existValue+existValue
+        ));
+        //4.想要自定义返回结果
+        Map<String,Integer> map4=stream.collect(Collectors.toMap(
+                i->i.toString(),
+                Function.identity(),
+                (existValue,newValue)->existValue+existValue,
+                TreeMap::new
+        ));
+
+    }
+    /**
+     * 分组
+     */
+    private void streamOperation_7(){
+        Stream<Integer> stream= Stream.of(0,1,2,3,4,5,6,7,8,9,10);
+        // Function函数接口 T->R 作为分类函数，得到相同结果的分在一组，以返回值为key，返回map<R,List<T>>;
+        //下面例子返回 0->(偶数)；1->(奇数)
+        Map<Integer,List<Integer>> map1=stream.collect(Collectors.groupingBy(
+                i->i%2
+        ));
+        //分类函数是一个predicate函数接口，根据校验结果分类
+        Map<Boolean,List<Integer>> map2=stream.collect(Collectors.partitioningBy(
+                i->i%2==0
+        ));
+        //自定义收集器
+        Map<Boolean,Set<Integer>> map3=stream.collect(Collectors.partitioningBy(
+                i->i%2==0,Collectors.toSet()
+        ));
+        //返回并发map
+        Map<Integer,List<Integer>> map4=stream.collect(Collectors.groupingByConcurrent(
+                i->i%2
+        ));
+        //其他收集器（收集器决定map的值的类型）
+        //Collectors.counting(),返回收集元素总数
+        //Collectors.summing(Int|double|long)(将元素转化为值的函数To..Function); 对分组元素调用To..Function()并聚合起来
+        //Collectors.maxBy(比较器)|minBy(比较器);生成分组元素中最大最小
+        //Collectors.mapping(映射函数，聚合函数);先对分组的元素映射操作，然后再聚合结果(返回的是数值类型)
+
+        //对分组的元素先映射然后进行一次普通的聚合操作，返回的结果可以是其他类型
+        //Collectors.reducing(初始值,映射函数，聚合函数);
     }
 
 }
